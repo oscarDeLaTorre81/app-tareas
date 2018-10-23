51e1b7375aea5fad9723de54e86a6248a992ddf7
oscarDeLaTorre81 <delatorreoscar81@gmail.com>
2018-10-23 21:59:14 +0500

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PieComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
